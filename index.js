c2eef7eedb1952baffb27c93d1ddcc4594e5ad25
utmichelle <54852282+utmichelle@users.noreply.github.com>
2019-11-02 17:31:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var json = {
             choices: [
                 "Sexual Misconduct", "Discrimination"
             ],
-           // colCount: 0
+            colCount: 1
         }, {
             //Sexual Misconduct
             type: "radiogroup",
